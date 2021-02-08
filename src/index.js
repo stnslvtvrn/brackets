febef559b1f65d4244b97f3240d1191102e9eee5
Stanislau <omgnoooob@gmail.com>
2021-02-08 12:45:20 +0300

    feat: complete brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let newArr = str.split('');
+  let iteration = newArr.length / 2;
+  for (let i = 0; i < iteration; i++) {
+    for (let j = 0; j < newArr.length; j++) {
+      if (newArr[j] == "{" && newArr[j + 1] == "}" || newArr[j] == "[" && newArr[j + 1] == "]" ||
+          newArr[j] == "(" && newArr[j + 1] == ")" || newArr[j] == "|" && newArr[j + 1] == "|" ||
+          newArr[j] == "1" && newArr[j + 1] == "2" || newArr[j] == "3" && newArr[j + 1] == "4" ||
+          newArr[j] == "5" && newArr[j + 1] == "6" || newArr[j] == "7" && newArr[j + 1] == "7" ||
+          newArr[j] == "8" && newArr[j + 1] == "8")
+          newArr.splice(j, 2);
+    }
+  }
+  return newArr.length == 0 ? true : false;
 }
+
